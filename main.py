5e1570168278af7ecde1f5d01e912308ebed06b9
Nithin Ravi <23nithinravi2003@gmail.com>
2025-04-30 02:04:37 +0530

    port-rectified

--- main.py
+++ main.py
@@ -61,4 +61,5 @@ def predict():
 
 # Run Flask app
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Use Render-provided port if available
+    app.run(host='0.0.0.0', port=port)
